fe6d6af3244f662aacbe8a7e1ec54cae68e8d5c4
Goodmanv4108 <61555814+Goodmanv4108@users.noreply.github.com>
2020-03-09 10:02:36 -0400

    Add files via upload
    This is how I made a graph.

---
+++ P2HW2_TurtleGraphic_GoodmanVeronica.py
@@ -0,0 +1,59 @@
+#This project is to create a turtle graph. I chose to create the circle graph.
+#2/18/2020
+#CTI-110 P2HW2 - Turtle Graphic
+#Veronica Goodman
+
+#Program Pseudocode
+
+#First Circle
+
+#Moving to location of circle
+import turtle
+turtle.penup()
+turtle.goto(-300, 50)
+turtle.pendown()
+#draw circle
+turtle.goto(-300, 50)
+turtle.circle(100)
+
+#Second Circle
+
+#Moving to location of circle
+turtle.penup()
+turtle.goto(-150,0)
+turtle.pendown()
+#draw circle
+turtle.goto(-150, 0)
+turtle.circle(100)
+
+#Third Circle
+
+#Moving to location of circle
+turtle.penup()
+turtle.goto(0, 50)
+turtle.pendown()
+#draw circle
+turtle.goto(0, 50)
+turtle.circle(100)
+
+#Fourth Circle
+
+#Moving to location of circle
+turtle.penup()
+turtle.goto(150, 0)
+turtle.pendown()
+#draw circle
+turtle.goto(150, 0)
+turtle.circle(100)
+
+#Fifth Circle
+
+#Moving to location of circle
+turtle.penup()
+turtle.goto(300, 50)
+turtle.pendown()
+#draw circle
+turtle.goto(300, 50)
+turtle.circle(100)
+
+
