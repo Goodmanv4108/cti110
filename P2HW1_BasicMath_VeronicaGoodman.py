34c85d6f2d0cf82e93d1a9326c5e54ec51ca3859
Goodmanv4108 <61555814+Goodmanv4108@users.noreply.github.com>
2020-03-09 10:03:00 -0400

    Add files via upload
    This is how I created basic math.

---
+++ P2HW1_BasicMath_VeronicaGoodman.py
@@ -0,0 +1,17 @@
+#This python code is to take two numbers from the user and multiply and add them.
+#2/21/2020
+#CTI-110 P2HW1 - Basic Math
+#Veronica Goodman
+#
+
+first_number = float(input('Enter the first number: '))
+
+second_number = float(input('Enter the second number: '))
+
+add_sum = first_number + second_number
+
+print('The sum of the numbers added together is', add_sum)
+
+multi_sum = first_number * second_number
+
+print('The sum of the numbers multiplied is', multi_sum)
