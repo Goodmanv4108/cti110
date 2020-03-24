ba18a5a1228753c0ffe7e91a3a2cf8191966a672
Goodmanv4108 <61555814+Goodmanv4108@users.noreply.github.com>
2020-03-23 20:15:52 -0400

    Add files via upload
    Creating my initials in turtle graph with python.

---
+++ P4T1B_Goodman.py
@@ -0,0 +1,28 @@
+import turtle
+#The details of the colors and pensize of the turtle graph.
+turtle.pensize(7)
+turtle.bgcolor('black')
+turtle.pencolor('white')
+#
+#The turtle graph for my first inital, V.
+turtle.goto(50, 100)
+turtle.goto(0, 0)
+turtle.goto(-50, 100)
+turtle.goto(0, 0)
+#Moving the pen/cursor to the next placement for the next initial.
+turtle.penup()
+turtle.goto(115, 100)
+turtle.pendown()
+#
+#The turtle graph for my first inital, G.
+turtle.goto(75, 100)
+turtle.goto(60, 73)
+turtle.goto(50, 50)
+turtle.goto(50, 25)
+turtle.goto(85, 0)
+turtle.goto(125, 25)
+turtle.goto(125, 35)
+turtle.goto(75, 35)
+turtle.goto(75, 25)
+#
+
