06a4910e95abf2a35f1e0ca962113c8aed8fda93
Goodmanv4108 <61555814+Goodmanv4108@users.noreply.github.com>
2020-03-23 20:17:02 -0400

    Add files via upload
    Correcting.

---
+++ P3LAB_Debugging_VeronicaGoodman.py
@@ -0,0 +1,33 @@
+#CTI-110
+#P3LAB-Debugging
+#Veronica Goodman
+#3/20/2020
+#
+
+#Getting test score
+#score = input("What is your test score?")
+
+#This program is to calculate the number score to the letter grade.    
+
+
+#Calculating the scores letter grade
+#
+
+if score >= A_Score:
+    print("Your grade is A.")
+    
+elif score >= B_Score:
+    print("Your grade is B.")
+    
+elif score >= C_Score:
+    print("Your grade is C.")
+    
+elif score >= D_Score:
+    print("Your grade is D.")
+    
+else:
+    print("Your grade is F.")
+
+#
+###I assume I do not need to make a working python code and just do the proper conventions of alignment and indentation like it says.
+
